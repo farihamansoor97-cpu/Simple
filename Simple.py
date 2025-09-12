998d05add1ff2300535db558e4dd5d546f4491a7
farihamansoor97-cpu <farihamansoor97@gmail.com>
2025-09-12 07:38:43 +0500

    Add files via upload

---
+++ Simple.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+day = "Friday,"
+date = "12th September 2025"
+program = "Python Programming"
+greeting = "Have a nice day!"
+
+print(f"Assalamualaikum! {name}, today is {day} {date}. Welcome to {program}. {greeting}")
